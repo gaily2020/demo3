208ed0f7d87ceb05ef36783d864589631b93f565
gaily2020 <980092226@qq.com>
2020-11-21 22:59:54 +0800

    用ssh-key方式提交到master

---
+++ src/main/java/com/gaily/Person.java
@@ -0,0 +1,19 @@
+package com.gaily;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.junit.Test;
+import org.w3c.dom.ls.LSOutput;
+
+public class Person {
+
+    public Person() {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void say(){
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+    }
+}
---
+++ src/main/java/com/gaily/User.java
@@ -0,0 +1,17 @@
+package com.gaily;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+import java.util.Date;
+
+/**
+ * 新增实体类
+ *
+ */
+
+public class User {
+    private Integer userId;
+    private String userName;
+    private Integer age;
+    private Date birthday;
+}
